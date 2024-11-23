6d25e2e00e8d8fc24c9260e4d3dc66c7722af88e
karpitony <watemark1017@gmail.com>
2024-11-23 21:36:38 +0000

    feat: 화면크기 md 이하는 루트 접근시 Home 띄우기

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,28 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import MainPageHeader from './MainPageHeader';
 import CommonHeader from './CommonHeader';
 
 export default function Header() {
   const location = useLocation();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   // 루트 페이지인지 확인
   const isRootPage = location.pathname === '/';
 
   return (
     <>
-      {isRootPage ? <MainPageHeader /> : <CommonHeader />}
+      {isRootPage ? (isMobile ? <CommonHeader /> : <MainPageHeader />) : <CommonHeader />}
     </>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,10 @@
+import { useEffect, useState } from 'react';
 import MyTask from '../components/MainPage/MyTask';
 import RecommendedTasks from '../components/MainPage/RecommendedTasks';
+import Home from './Home';
 
 export default function MainPage() {
+  const [isMobile, setIsMobile] = useState(false);
   const userTasks = Array.from({ length: 4 }).map((_, index) => ({
     title: `사용자 작업 ${index + 1}`,
     description: '',
@@ -12,17 +15,31 @@ export default function MainPage() {
     description: '브랜드 로고 디자인 및 기획서 제안',
   }));
 
-  return (
-    <div className="w-full bg-white flex flex-col md:flex-row px-10 gap-10">
-      {/* MyTask 컴포넌트 */}
-      <div className="flex lg:w-96">
-        <MyTask userTasks={userTasks} />
-      </div>
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
-      {/* RecommendedTasks 컴포넌트 */}
-      <div className="flex-1 ">
-        <RecommendedTasks tasks={recommendedTasks} />
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <>
+    {isMobile ? <Home /> : (
+      <div className="w-full bg-white flex flex-col md:flex-row px-10 gap-10">
+        {/* MyTask 컴포넌트 */}
+        <div className="flex lg:w-96">
+          <MyTask userTasks={userTasks} />
+        </div>
+
+        {/* RecommendedTasks 컴포넌트 */}
+        <div className="flex-1 ">
+          <RecommendedTasks tasks={recommendedTasks} />
+        </div>
       </div>
-    </div>
+    )}
+    </>
   );
 }
